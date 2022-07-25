e6f74b995a69b4209982de5d9cf4a7780cf3e622
Quentin <quentin.boutet2020@campus-eni.fr>
2022-07-25 11:25:28 +0200

    [Hangout] filtrage des sorties

--- src/Form/CityFormType.php
+++ src/Form/CityFormType.php
@@ -4,16 +4,18 @@ namespace App\Form;
 
 use App\Entity\City;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CityType extends AbstractType
+class CityFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('zipCode')
+            ->add('name',TextType::class)
+            ->add('zipCode',IntegerType::class)
         ;
     }
 
